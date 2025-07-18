c13abeade3c34403dd3c04bc32c28280d12fef15
Sumbal Hashmi <sumbalhashmi14@gmail.com>
2025-07-19 00:03:33 +0500

    single num

---
+++ SingleNumber/index.cpp
@@ -0,0 +1,13 @@
+// Problem 136 Single Number 
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int i : nums) {
+         ans = ans ^ i;
+        }
+
+        return ans;
+    }
+};
