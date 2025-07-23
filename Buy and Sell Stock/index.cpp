935c19fe6bc7a765a2b452083109cebb0b50af0a
Sumbal Hashmi <sumbalhashmi14@gmail.com>
2025-07-23 16:13:33 +0500

    solved problem 121

---
+++ Buy and Sell Stock/index.cpp
@@ -0,0 +1,20 @@
+//Prolem- 121 Best Time To Buy and Sell Stock
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit  = 0;
+        int bestBuy = prices[0];
+
+        for(int i = 1; i<prices.size(); i++) {
+           if(prices[i] > bestBuy) {
+             maxProfit = max(maxProfit, prices[i] - bestBuy);
+           }
+              bestBuy = min(bestBuy, prices[i]);
+        };
+
+        return maxProfit;
+    }
+
+    
+};
