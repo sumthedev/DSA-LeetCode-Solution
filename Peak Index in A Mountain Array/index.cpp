0a0b62f14ef746cd52d035106bdb5ef060acc787
Sumbal Hashmi <sumbalhashmi14@gmail.com>
2025-07-28 19:02:05 +0500

    fix runtime error on leetcode pro

--- Peak Index in A Mountain Array/index.cpp
+++ Peak Index in A Mountain Array/index.cpp
@@ -3,20 +3,20 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& A) {
-        int n = A.size();
-        int s = 1;
-        int e = n-2;
+
+        int s = 1; 
+        int e = A.size() - 2; 
 
         while(s <= e) {
             int mid = s + (e-s)/2;
-            if(A[mid] > A[mid-1] && A[mid] > A[mid+1]) {
+            if(A[mid -1] < A[mid] && A[mid] > A[mid+1]) {
                 return mid;
-            } 
-            if(A[mid] < A[mid-1]) {
-                e = mid-1;
+            } else if (A[mid-1] < A[mid]) {
+               s = mid+1;
             } else {
-                s = mid-1;
+             e = mid-1;
             }
+          
         } 
 
         return -1;
