62cc919a3520c3b58573c45091c76c15a9584d09
Sumbal Hashmi <sumbalhashmi14@gmail.com>
2025-07-29 17:20:17 +0500

    solved problem 540 again

--- Single Element In Sorted Array/index.cpp
+++ Single Element In Sorted Array/index.cpp
@@ -3,11 +3,12 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& A) {
-        int n = A.size() -1;
+        int n = A.size();
+       
+        if(n == 1) return A[0];
+
         int st = 0;
         int end = n-1;
-
-        if(n==1) return A[0];
 
         while(st <= end) {
             int mid = st + (end - st)/2;
@@ -18,21 +19,22 @@ public:
             };
 
             if(mid%2 == 0){ 
-                if(A[mid-1] == mid) {
+                if(A[mid-1] == A[mid]) {
                     end = mid - 1;
                 } else {
                     st = mid+1;
                 }
 
             } else {
-                 if(A[mid-1] == mid) {
+                 if(A[mid-1] == A[mid]) {
                       st = mid+1;
                 } else {
                      end = mid - 1;
                 }
             }
         }
-         return -1;
+      
+      return -1;
 
     }
 };
