089f2ff52df3f3b181f49cc1a6de9661b17bc668
Sumbal Hashmi <sumbalhashmi14@gmail.com>
2025-09-12 20:15:01 +0500

    again solved with set

--- Combinations Sum/index.cpp
+++ Combinations Sum/index.cpp
@@ -28,3 +28,45 @@ public:
         
     }
 };
+
+//second sol with sets 
+
+class Solution {
+public:
+
+ set<vector<int>> s;
+  void helper (vector<int>& arr, int i, int tar,  vector<vector<int>>& ans, vector<int>& comb)  {
+  
+  int n = arr.size();
+    if( i== n || tar < 0) {
+    return;
+  }
+
+  if(tar == 0) {
+    if(s.find(comb) == s.end()) {
+       ans.push_back(comb);
+       s.insert(comb);
+    }
+   
+    return;
+  }
+
+
+
+  comb.push_back(arr[i]);
+  helper(arr, i+1,  tar-arr[i], ans, comb);
+  helper(arr, i, tar-arr[i], ans, comb);
+  comb.pop_back();
+  helper(arr, i+1, tar, ans, comb);
+
+  }
+    vector<vector<int>> combinationSum(vector<int>& arr, int tar) {
+    vector<vector<int>> ans;
+    vector<int> comb;
+  
+
+    helper(arr, 0, tar, ans, comb);
+    return ans;
+        
+    }
+};
